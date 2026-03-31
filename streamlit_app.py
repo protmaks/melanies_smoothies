8fdd1d3e3377c7047f5912abe59ade37514d12e3
Maksim Pachkouski <115699713+protmaks@users.noreply.github.com>
2026-03-31 17:42:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,17 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.sql("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.public.fruit_options")
+my_dataframe = session.sql("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.public.fruit_options").to_pandas()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe
 #st.dataframe(pd_df)
 #st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
     my_dataframe['FRUIT_NAME'].tolist(),
-    key='smoothie_ingredients' # This makes the ID unique
+    key='smoothie_ingredients'
 )
 
 if ingredients_list:
